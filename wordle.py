834fea05737d128d773812aa9f69d2e967620091
Kevin <your_uni@columbia/barnard.edu>
2024-02-19 23:03:17 -0500

    Top-level design complete

--- wordle.py
+++ wordle.py
@@ -1,3 +1,10 @@
+from colorama import Back
+def print_green(letter):
+    print(Back.GREEN + f" {letter} " + Back.RESET, end='')
+def print_yellow(letter):
+    print(Back.LIGHTYELLOW_EX + f" {letter} " + Back.RESET, end='')
+def print_gray(letter):
+    print(Back.WHITE + f" {letter} " + Back.RESET, end='')
 
 def main():
     word = get_word()
