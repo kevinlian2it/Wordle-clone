a1e9ed10ca358c47c558c9a34903adb9d035c37c
Kevin <your_uni@columbia/barnard.edu>
2024-02-20 00:33:42 -0500

    Homework Complete

--- wordle.py
+++ wordle.py
@@ -14,32 +14,35 @@ def main():
         # Remove newline characters and any potential whitespace around the words
         words = [word.strip() for word in words]
     word = get_word(words)
-    for i in range(6):
-        guess = input()
+    i = 1
+    while i<=6:
+        guess = input(f"Guess {i}/6: ")
         if guess not in words:
             print("Enter a valid guess!")
-            i-=1
         else:
-            print("Guess " + str(i+1) + "/6: " + guess)
             check_guess(word,guess)
+            i+=1
         if guess == word:
-            print("You guessed right!\n")
-            exit()
-    print("You failed, try again next time.")
+            print("You guessed right!")
+            return 0
+    print("You failed, try again next time. The word was " + word + ".")
+    return 0
 
 def get_word(words):
     return random.choice(words)
 
 def check_guess(word, guess):
-    for i in range(5):
-        if guess[i] in word:
-            if guess[i] == word[i]:
-                print_green(guess[i])
-            else:
-                print_yellow(guess[i])
+    word_list = list(word)  # Convert word to a list to allow for mutable operations
+    for i in range(len(guess)):
+        if guess[i] == word_list[i]:
+            print_green(guess[i])
+            word_list[i] = None  # Mark this position as correctly guessed
+        elif guess[i] in word_list:
+            print_yellow(guess[i])
+            word_list[word_list.index(guess[i])] = None  # Mark this letter as used
         else:
             print_gray(guess[i])
-    return 0
+    print()  # Add a newline for better readability
 
 if __name__ == '__main__':
     main()
